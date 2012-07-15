0280c4ccd64f98747bff9dfb5686d6a8d5c0dc95
Brandon McMullin <brandon.m.mcmullin@gmail.com>
2012-07-14 17:01:35 -0700

    architecture refactor; added live feed
    got rid of the iframe architecture, opting for front end javascript to
    dynamically add content. switched hard coded example news items over to
    actual Bungie.net rss items

--- scripts/bc-interface.js
+++ scripts/bc-interface.js
@@ -1,11 +1,75 @@
+window.bcInterface = {};
+
+bcInterface.rssUrl = "http://www.bungie.net/News/NewsRss.ashx";
+bcInterface.twitterFeedUrl = "http://twitter.com/statuses/user_timeline/bungie.rss"
+
+bcInterface.renderSelectedView = function(pageId) {
+	switch (pageId) {
+		case "news": {
+			bcInterface.renderNewsFeed();
+		} break;
+		case "profile": {
+		} break;
+		default:{
+		} break;
+	}
+	return false;
+};
+
+bcInterface.renderNewsFeed = function() {
+
+	$.ajax({
+		url: bcInterface.rssUrl,
+		type:"GET",
+		dataType:"XML",
+		success:function(data) {
+			$('#bc-content').append("<ul id='bc-news' class='bc-news-list'></ul>");
+			
+			var newsData = $(data).find('item');
+
+			$.ajax({
+					url: bcInterface.twitterFeedUrl,
+					type:"GET",
+					dataType:"XML",
+					success:function(twitterData) {
+						newsData = newsData.concat($(twitterData).find('item'));
+					}
+			});					
+			
+			$(newsData).each(function() {	
+			
+				var $item = $(this);
+				
+				var title = $item.find('title').text();
+				var link = $item.find('link').text();
+				var img = (link.indexOf("twitter") > -1) ? "images/twitter.gif" : "images/bnet.gif";
+				
+				var pubDate = $item.find('pubDate').text();
+				
+				var htmlString = "<li class='bc-news-item' exRef='" + link + "'><img src='" + img + "' /><ul class='bc-news-details' ><li><span>"+ title + "</span></li>" +
+				                 "<li class='bc-news-pub-date'><span>" + pubDate + "</span</li></ul></li>";
+				
+				$('#bc-news').append(htmlString);	
+			});
+			
+			$('.bc-news-item ').click(function() {
+				window.open($(this).attr('exref'));
+			});
+		}
+		
+	});
+	return false;
+};
+
 $(document).ready(function() {
 	$('.bc-nav-item').click(function() {
 		$('.bc-nav-item').removeClass('bc-nav-active');
 		$(this).addClass('bc-nav-active');
-		$('#bc-content-body').attr('src', $(this).attr('intref'));
+		$('#bc-content').children().remove();
+		bcInterface.renderSelectedView($(this).attr('intRef'));
 	});
 	
-	$('.bc-news-list li').click(function() {
-		window.open($(this).attr('exref'));
-	});
+	//generically discover the default view then render it
+	bcInterface.renderSelectedView("news");
+
 });
