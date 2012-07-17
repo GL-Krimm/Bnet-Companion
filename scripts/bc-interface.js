9955d8334456c333f90faf200a55f80e0d34aedd
Brandon McMullin <brandon.m.mcmullin@gmail.com>
2012-07-17 01:02:05 -0700

    combining twitter feed, sorted by date

--- scripts/bc-interface.js
+++ scripts/bc-interface.js
@@ -16,57 +16,30 @@ bcInterface.renderSelectedView = function(pageId) {
 	return false;
 };
 
-bcInterface.renderNewsFeed = function(data) {
+bcInterface.renderNewsFeed = function(newsData) {
 
 	$('#bc-content').append("<ul id='bc-news' class='bc-news-list'></ul>");
-	
-	var newsData = $(data).find('item');				
-	
-	$(newsData).each(function() {	
-	
-		var $item = $(this);
+	//var newsData = bcInterface.sortNewsFeed(data);
+	newsData = JSON.parse(newsData);
+	for ( var i = 0; i < newsData.length; i++ )
+	{
+		var title = newsData[i].title;
 		
-		var title = $item.find('title').text();
-		var link = $item.find('link').text();
-		var img = (link.indexOf("twitter") > -1) ? "images/twitter.gif" : "images/bnet.gif";
-		
-		var pubDate = $item.find('pubDate').text();
-		
-		var htmlString = "<li class='bc-news-item' exRef='" + link + "'><img src='" + img + "' /><ul class='bc-news-details' ><li><span>"+ title + "</span></li>" +
-						 "<li class='bc-news-pub-date'><span>" + pubDate + "</span</li></ul></li>";
+		if (title.length > 55 ) {
+			title = $.trim(title).substring(0,55).split(" ").slice(0, -1).join(" ") + "...";
+		}
+	
+		var img = (newsData[i].link.indexOf("twitter") > -1) ? "images/twitter.gif" : "images/bnet.gif";
+		var htmlString = "<li class='bc-news-item' exRef='" + newsData[i].link + "'><img src='" + img + "' /><ul class='bc-news-details' ><li><span>"+ title + "</span></li>" +
+						 "<li class='bc-news-pub-date'><span>" + newsData[i].pubDate + "</span</li></ul></li>";
 		
 		$('#bc-news').append(htmlString);	
-	});
+	}
 	
 	$('.bc-news-item ').click(function() {
 		window.open($(this).attr('exref'));
 	});
 }
-
-bcInterface.sortNewsFeed = function(feedData) {
-	var bnetRss = feedData.rss;
-	var bungieTwitter = feedData.twitter;
-	
-	// master array is the bnet news feed, it gets higher priority
-	var newsData = $(bnetRss).find('item');
-	
-	// secondary array is the twitter feed, which is lower priority
-	var twitterData = $(bungieTwitter).find('item');
-	
-	// sort the entries by date by merging elements into the master array
-	// by date; for date collisions bnet posted items have higher priority
-	// and go first on the list
-	
-	var lastMasterNode = 0;
-	
-	for ( i = 0; i < twitterData.length; i++ ) {
-		for ( x = lastMasterNode; x < newsData.length; x++ ) {
-			var bDate = new Date(newsData[x]);
-			var tDate = new Date(twitterData[i]);
-		} 
-	}
-	
-};
 
 $(document).ready(function() {
 	$('.bc-nav-item').click(function() {
